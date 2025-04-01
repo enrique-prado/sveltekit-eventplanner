5ea7e76192b01ec3937c790576dfa6bb3cece654
Enrique Prado <enrique.prado7@gmail.com>
2025-04-01 08:25:55 -0700

    Layout tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,35 +4,34 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<h1 class="text-xl">Events</h1>
-{#await data.events}
-    <p>Loading events...</p>
-{:then events}
-    <table class="table-auto">
-        <thead>
-            <tr>
-                <th class="px-4 py-2">ID</th>
-                <th class="px-4 py-2">Title</th>
-                <th class="px-4 py-2">Description</th>
-                <th class="px-4 py-2">Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each events as event}
-                <tr>
-                    <td class="border px-4 py-2">{event.id}</td>
-                    <td class="border px-4 py-2">{event.title}</td>
-                    <td class="border px-4 py-2">{event.description}</td>
-                    <td class="border px-4 py-2">{event.date}</td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-{:catch error}
-    <p>Error loading events: {error.message}</p>
-{/await}
-
+<h1 class="text-xl text-center">Event Planner</h1>
 <a class="btn" href="/newevent" role="button">Add Event</a>
+<div class="flex justify-center">
+	{#await data.events}
+		<p>Loading events...</p>
+	{:then events}
+		<table class="table-auto">
+			<thead>
+				<tr>
+					<th class="px-4 py-2">ID</th>
+					<th class="px-4 py-2">Title</th>
+					<th class="px-4 py-2">Description</th>
+					<th class="px-4 py-2">Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each events as event}
+					<tr>
+						<td class="border px-4 py-2">{event.id}</td>
+						<td class="border px-4 py-2">{event.title}</td>
+						<td class="border px-4 py-2">{event.description}</td>
+						<td class="border px-4 py-2">{event.date}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:catch error}
+		<p>Error loading events: {error.message}</p>
+	{/await}
+	
+</div>
